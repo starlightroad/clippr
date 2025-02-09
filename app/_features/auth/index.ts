c82e6509409aaf75210a5833b80707dd90429479
starlightroad <ortizge92@gmail.com>
2025-02-09 00:07:02 -0500

    Build the registration form schema using Zod

--- app/_features/auth/index.ts
+++ app/_features/auth/index.ts
@@ -1,1 +1,9 @@
+export {
+  MAX_PASSWORD,
+  MIN_PASSWORD,
+  PASSWORD_REGEX,
+} from "@/app/_features/auth/lib/constants";
+
+export { RegistrationFormSchema } from "@/app/_features/auth/lib/definitions";
+
 export { default as RegistrationForm } from "@/app/_features/auth/components/registration-form";
