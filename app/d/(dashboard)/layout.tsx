81e549ab7c9f7e69cfa9b245646f3521743e6351
starlightroad <ortizge92@gmail.com>
2025-03-05 19:28:19 -0600

    Build the dashboard layout and the navbar

---
+++ app/d/(dashboard)/layout.tsx
@@ -0,0 +1,21 @@
+import AppSidebar from "@/app/_components/sidebar";
+import { Navbar } from "@/app/_features/dashboard";
+import { SidebarProvider } from "@/app/_components/ui/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="w-full bg-sidebar p-2">
+        <div className="h-full w-full rounded-md border bg-white">
+          <Navbar />
+          {children}
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+}
